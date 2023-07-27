666b5e43ab1a0f224197de4015f4294b50050712
kellybeamsley <kellybeamsley@gmail.com>
2023-07-27 15:19:06 -0500

    2 prompt - Favorite color

--- js/external.js
+++ js/external.js
@@ -1,3 +1,8 @@
 console.log('Hello from external JavaScript');
 
 alert("Welcome to my Website!");
+
+var userInput = prompt('What is your favorite color:');
+console.log('Your favorite color is ' + userInput);
+
+alert("Great, " + userInput + " is my favorite color too!");

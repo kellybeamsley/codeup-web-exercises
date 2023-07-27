b866d9a423bcd681d131011fd8d8dde2e561751a
kellybeamsley <kellybeamsley@gmail.com>
2023-07-27 16:27:28 -0500

    20230727 Google-Amazon-Facebook Exercise - kb

--- js/external.js
+++ js/external.js
@@ -7,6 +7,8 @@ console.log('Your favorite color is ' + userInput);
 
 alert("Great, " + userInput + " is my favorite color too!");
 
+
+
 // Define variables for the number of days each movie is rented
 let littleMermaidDays = prompt('How many rental days do you want the movie, The Little Mermaid?');
 console.log('You want The Little Mermaid for ' + littleMermaidDays + " days");
@@ -29,3 +31,32 @@ var totalCost = littleMermaidCost + brotherBearCost + herculesCost;
 
 // Display the result
 alert("Total amount to pay: $" + totalCost);
+
+
+
+// Prompt variables for the cost of hour rate
+let googleHourRate = prompt('What is the hourly rate for a Google contractor?');
+console.log('Your cost per hour at Google is $' + googleHourRate + '.');
+let amazonHourRate = prompt('What is the hourly rate for an Amazon contractor?');
+console.log('Your cost per hour at Amazon is $' + amazonHourRate + '.');
+let facebookHourRate = prompt('What is the hourly rate for a Facebook contractor?');
+console.log('Your cost per hour at Facebook is $' + facebookHourRate + '.');
+
+// Prompt variables for the hours worked
+let googleHoursWorked = prompt('How many hours did you work at Google this week?');
+console.log('You worked ' + googleHoursWorked + ' hours this week at Google.');
+let amazonHoursWorked = prompt('How many hours did you work at Amazon this week?');
+console.log('You worked ' + amazonHoursWorked + ' hours this week at Amazon.');
+let facebookHoursWorked = prompt('How many hours did you work at Facebook this week?');
+console.log('You worked ' + facebookHoursWorked + ' hours this week at Facebook.');
+
+// Calculate the total payment for each company
+let googlePayment = googleHourRate * googleHoursWorked;
+let amazonPayment = amazonHourRate * amazonHoursWorked;
+let facebookPayment = facebookHourRate * facebookHoursWorked;
+
+// Calculate the total payment for this week as a contractor
+let totalPayment = googlePayment + amazonPayment + facebookPayment;
+
+// Display the result
+alert("Total payment for this week: $" + totalPayment);

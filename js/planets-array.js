f019d4cd9d0d7a5142346f7e5e0abf211cf6431d
kellybeamsley <kellybeamsley@gmail.com>
2023-08-07 17:04:57 -0500

    20230807 Planet Array Exercise 07AUG2023-kb

--- js/planets-array.js
+++ js/planets-array.js
@@ -1,7 +1,7 @@
 (function(){
     "use strict";
 
-    var planets = [
+    let planets = [
         'Mercury',
         'Venus',
         'Earth',
@@ -20,21 +20,51 @@
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+    planets.unshift('Sun');
+    
 
+    
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
+    planets.push('Pluto');
+
+
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
+    planets.shift('Sun');
+
+
 
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
+    planets.pop('Pluto');
+
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    let index = planets.indexOf('Earth');
+    console.log(index);
+
+
 
     console.log("Reversing the order of the planets array.");
     console.log(planets);
 
+    console.log('Reversing the planets array.');
+
+    planets.reverse();
+
+    console.log(planets);
+
+
+
     console.log("Sorting the planets array.");
     console.log(planets);
+
+    console.log('Sorting the planets array.')
+
+    planets.sort();
+    console.log(planets);
+
+
 })();
